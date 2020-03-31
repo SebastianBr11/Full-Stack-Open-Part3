d109aa9fde9ca4a55ccfe519ad4ee8350b1ee530
SebiPlayzZ <41546861+SebiPlayzZ@users.noreply.github.com>
2020-03-31 11:38:39 +0200

    step7 - step8

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('object', (req, res) => JSON.stringify(req.body))
+
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :object'
+  )
+)
 
 let persons = [
   {
